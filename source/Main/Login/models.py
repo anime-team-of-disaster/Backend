cec4f171c7b0fcbcefcd6b0f73696882980b3d7a
mixmg <mixmg1@gmail.com>
2019-09-10 17:29:59 +0200

    addin registration

--- source/Main/Login/models.py
+++ source/Main/Login/models.py
@@ -3,6 +3,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
