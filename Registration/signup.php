694db961bb58f327c579b2721388eb4891511e0e
mixmg <mixmg1@gmail.com>
2019-09-12 14:05:19 +0200

    registration commit

---
+++ Registration/signup.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Rejestracja użytkownika</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	</head>
+	<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 offset-md-4 form-div">
+				<form action="signup.php" method="post">
+				<h3 class="text-center">Rejestracja</h3>
+
+				<div class="form-group">
+					<label for="username">Nazwa użytkownika</label>
+					<input type="text" name="username" class="form-control form-control-lg">
+				</div>
+
+				<div class="form-group">
+					<label for="Email">Email</label>
+					<input type="Email" name="Email" class="form-control form-control-lg">
+				</div>
+
+				<div class="form-group">
+					<label for="Password">Hasło</label>
+					<input type="Password" name="Password" class="form-control form-control-lg">
+				</div>
+
+				<div class="form-group">
+					<label for="Password_2">Powtórz Hasło</label>
+					<input type="Password_2" name="Password_2" class="form-control form-control-lg">
+				</div>
+
+				<div class="form-group">
+				<button type="submit" name="signup-btn" class="btn btn-primary btn-block btn-lg">Zarejestruj się!</button>
+				</div>
+				<p class="text-center">Zarejestrowany? <a href="login.php">Zaloguj się.</a></p>
+
+				</form>
+			</div>
+		</div>
+	</div>
+
+	</body>
+</html>
