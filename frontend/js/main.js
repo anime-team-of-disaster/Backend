590b490378cfe2a7d2d716ab59191543af24b27c
robertpurc <robert.purc12@gmail.com>
2019-10-06 10:54:29 +0200

    swiper working sandbox

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -7,3 +7,39 @@ btn.addEventListener("click", function() {
 
   span.classList.toggle("active--span");
 });
+
+//slider
+
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // init: false,
+  pagination: {
+    el: ".swiper-pagination"
+    // clickable: true
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50
+    },
+    1400: {
+      slidesPerView: 5,
+      spaceBetween: 50
+      // grabCursor: true
+    }
+  }
+});
